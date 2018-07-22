1f386237d76a61823d3f3c40b5c549842628a3dc
ssc在路上 <2698460477@qq.com>
2018-07-22 16:15:14 +0800

    ssc在路上router学习

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JokesComponent } from './jokes/jokes.component';
+import { PictureComponent } from './picture/picture.component';
+import { GifComponent } from './picture/gif/gif.component';
+import { JpgComponent } from './picture/jpg/jpg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JokesComponent,
+    PictureComponent,
+    GifComponent,
+    JpgComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/picture/jpg/jpg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jpg',
+  templateUrl: './jpg.component.html',
+  styleUrls: ['./jpg.component.css']
+})
+export class JpgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
